836bcaf7bc39a0a3efa946b625ded1eaad6eb5df
rajat sharma <108734280+rajat-cloudtechner@users.noreply.github.com>
2022-07-26 12:23:00 +0530

    Update awscli.sh

--- awscli.sh
+++ awscli.sh
@@ -114,7 +114,7 @@ aws ec2 authorize-security-group-ingress --group-id $SG_ID --protocol tcp --port
 aws ec2 authorize-security-group-ingress --group-id $SG_ID --protocol tcp --port 8080 --cidr 0.0.0.0/0
 
 # Create key-pair for private subnet
-aws ec2 create-key-pair --key-name $PRIVATE_KP --query 'KeyMaterial' --output text > $PRIVATE_KP.pem
+#aws ec2 create-key-pair --key-name $PRIVATE_KP --query 'KeyMaterial' --output text > $PRIVATE_KP.pem
 
 #creating ec2 in private-subnet
 INSTANCE_ID_2=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --key-name $PRIVATE_KP --security-group-ids $SG_ID --subnet-id $SUBNET_PRIVATE_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MyPrivateInstance}]' --user-data file://docker.sh --region $AWS_REGION)
@@ -123,7 +123,7 @@ echo "INSTANCE ID 2 '$INSTANCE_ID_2' CREATED"
 sleep 1m
 
 # Create key-pair for public subnet
-aws ec2 create-key-pair --key-name $PUBLIC_KP --query 'KeyMaterial' --output text > $PUBLIC_KP.pem
+#aws ec2 create-key-pair --key-name $PUBLIC_KP --query 'KeyMaterial' --output text > $PUBLIC_KP.pem
 
 #creating ec2 in public-subnet
 INSTANCE_ID_1=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --key-name $PUBLIC_KP --security-group-ids $SG_ID --subnet-id $SUBNET_PUBLIC_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MyPublicInstance}]' --user-data file://docker.sh --region $AWS_REGION)
