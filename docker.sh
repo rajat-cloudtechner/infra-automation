7e409ba39de97bf9c320124e0ce8746a97fefbaa
rajat sharma <108734280+rajat-cloudtechner@users.noreply.github.com>
2022-07-28 10:58:02 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo yum update -y
-sudo yum search docker
-sudo yum install docker -y
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
-sudo usermod -a -G docker ec2-user
-sudo chmod 666 /var/run/docker.sock
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
