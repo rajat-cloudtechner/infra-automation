44cc73616a1c88fa754e54fe97e74447f36e035f
rajat sharma <108734280+rajat-cloudtechner@users.noreply.github.com>
2022-07-28 08:52:38 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-sudo yum update
+sudo yum update -y
 sudo yum search docker
-sudo yum install docker
+sudo yum install docker -y
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
+sudo usermod -a -G docker ec2-user
+sudo chmod 666 /var/run/docker.sock
